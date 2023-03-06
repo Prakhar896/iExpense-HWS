e8ea9ddade65b84491102c3abe89459a64c9014c
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-03-06 18:49:34 +0800

    added data persistence in models file

--- iExpense/Models.swift
+++ iExpense/Models.swift
@@ -7,13 +7,31 @@
 
 import Foundation
 
-struct ExpenseItem: Identifiable { // conformance to identifiable requires an id property that is a type of ObjectIdentifier, which UUID is.
-    let id = UUID()
+struct ExpenseItem: Identifiable, Codable { // conformance to identifiable requires an id property that is a type of ObjectIdentifier, which UUID is.
+    var id = UUID()
     let name: String
     let type: String
     let amount: Double
 }
 
 class Expenses: ObservableObject {
-    @Published var items: [ExpenseItem] = []
+    @Published var items: [ExpenseItem] = [] {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "ExpenseItems")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) { // [ExpenseItem].self tells Swift that we are not creating a copy of an array of expense items or doing anything else but that we are decoding from an idea of an array of expense items
+                items = decodedItems
+                return
+            }
+        }
+        
+        // Error cases come here
+        items = []
+    }
 }
